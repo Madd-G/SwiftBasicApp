026a4ee094b0a48d875a93c9ba5d125b59da9296
Madd-G <akhmadnuralamsyah@gmail.com>
2024-11-24 18:35:32 +0700

    Initial Commit

---
+++ BasicApp/BasicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAppApp.swift
+//  BasicApp
+//
+//  Created by Akhmad Nur Alamsyah on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
