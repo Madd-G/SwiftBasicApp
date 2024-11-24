f21c2d0cf73ba4bfa62aebfb0d49cb11db0abf7c
Madd-G <akhmadnuralamsyah@gmail.com>
2024-11-24 19:57:42 +0700

    Create Custom View and Data Model

---
+++ BasicApp/DataItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataItemModel.swift
+//  BasicApp
+//
+//  Created by Akhmad Nur Alamsyah on 24/11/24.
+//
+
+import Foundation
---
+++ BasicApp/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  BasicApp
+//
+//  Created by Akhmad Nur Alamsyah on 24/11/24.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextView()
+}
