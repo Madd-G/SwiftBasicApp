97ff8d55d996080d271bc10b3ed80424ec142882
Madd-G <akhmadnuralamsyah@gmail.com>
2024-11-25 20:46:39 +0700

    Change button color when tapped

--- BasicApp/TextView.swift
+++ BasicApp/TextView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct TextView: View {
     let text: String
-    let color: Color
+    @State var color: Color
+    
+    let colors: [Color] = [
+        .red,
+        .green,
+        .blue,
+        .yellow,
+        .orange,
+        Color(red: 0.5, green: 0, blue: 0),
+        Color(red: 0, green: 0.5, blue: 0.5),
+        Color(red: 139/255, green: 207/255, blue: 240/255),
+        Color(red: 1, green: 215/255, blue: 0),
+    ]
     var body: some View {
         Text(text)
             .fontWeight(.semibold)
@@ -18,6 +30,9 @@ struct TextView: View {
             .background(color.opacity(0.4))
             .cornerRadius(20)
             .shadow(color: color, radius: 5, x: 10.0, y: 10.0)
+            .onTapGesture {
+                color = .red
+            }
     }
 }
 
