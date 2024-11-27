5edc0d3a1a6e364ba17cdf8fca35145067798707
Madd-G <akhmadnuralamsyah@gmail.com>
2024-11-27 10:53:27 +0700

    define color for iOS 16 and below

---
+++ BasicApp/Color Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Color Constants.swift
+//  BasicApp
+//
+//  Created by Akhmad Nur Alamsyah on 27/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+// for iOS 16 or below
+extension Color {
+    static let newRed = Color("myRed")
+    static let newPurple = Color("myPurple")
+    
+}
+
